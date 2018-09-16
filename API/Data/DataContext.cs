77d1a79cf64bfa02eef251ebc27abac7454ae207
seagull <mcit.alex85@gmail.come>
2018-09-16 22:45:39 +0200

    Initial Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data {
+    public class DataContext : DbContext {
+
+        public DbSet<Value> Values { get; set; }
+        public DataContext (DbContextOptions<DataContext> options) : base (options) {
+
+        }
+    }
+}
